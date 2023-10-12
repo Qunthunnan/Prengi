c7e498e1de70c3d0aaf6a4f795b4c7379f748574
Qunthunnan <bredtv6@gmail.com>
2023-10-13 01:10:46 +0300

    12.10.2023 slider-tab products - done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-new Splide( '.splide' ).mount();
+const mainSlider = new Splide( '.splide[aria-label="mainSlider"]' ),
+      productsSlider = new Splide('.splide[aria-label="productsSlider"]'),
+      productsButtons = ['Prengi Production', 'Prengi FMC', 'Prengi Mallz', 'Retail Prengi', 'Logistic Prengi', 'IT Prengi HR'];
+
+
+mainSlider.mount();
+
+productsSlider.on( 'pagination:mounted', function ( data ) {
+    // You can add your class to the UL element
+    data.list.classList.add( 'splide__pagination--custom' );
+  
+    // `items` contains all dot items
+    data.items.forEach( function ( item) {
+      item.button.textContent = productsButtons[item.page];
+    } );
+  } );
+  
+
+productsSlider.mount();
