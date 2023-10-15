77667fa3eb7398731c6f3fa5a1fc5e03928e243d
Qunthunnan <bredtv6@gmail.com>
2023-10-16 00:07:46 +0300

    16.10.23 mobile adaptation header - done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,77 @@
-const mainSlider = new Splide( '.splide[aria-label="mainSlider"]' ),
-      productsSlider = new Splide('.splide[aria-label="productsSlider"]'),
-      productsButtons = ['Prengi Production', 'Prengi FMC', 'Prengi Mallz', 'Retail Prengi', 'Logistic Prengi', 'IT Prengi HR'];
+function phoneSmallEditing(phone) {
+	const availableChars = ['0','1','2','3','4','5','6','7','8','9','(',')','+'];
+	if(typeof(phone) == 'string') {
+		let result = '';
+		nextC: for(let i = 0; i < phone.length; i++) {
+					for(let k = 0; k < availableChars.length; k++) {
+						if(phone[i] == availableChars[k]) {
+							result+=phone[i];
+							continue nextC;
+						}
+					}
+	}
+	return result
+  } 
+  return 'error';
+}
 
+function headerSmallPhoneAdaptation (headerPhonesElements) {
+	headerPhonesElements.forEach(item => {
+		item.innerText = phoneSmallEditing(item.innerText);
+	});
+}
+
+function headerNormalPhoneAdaptation (headerPhonesElements, headerPhones) {
+	headerPhonesElements.forEach((item, i) => {
+		item.innerHTML = headerPhones[i];
+	});
+}
+
+const headerPhonesElements = document.querySelectorAll('.header__country-text-tel'),
+	  headerPhones = [];
+headerPhonesElements.forEach((item, i) => {
+	headerPhones[i] = item.innerText;
+});
+const smallDisplay = window.matchMedia('(max-width: 991px)'),
+	  normalDisplay = window.matchMedia('(min-width: 992px)'),
+	  mobileHeaderButton = document.querySelector('.header__mobile-button');
+
+smallDisplay.addEventListener('change', (e)=> {
+	if (e.matches) {
+		console.log('matchSmall!');
+		headerSmallPhoneAdaptation(headerPhonesElements);
+	}
+});
+normalDisplay.addEventListener('change', (e)=> {
+	if (e.matches) {
+		console.log('normalMatch!');
+		headerNormalPhoneAdaptation(headerPhonesElements, headerPhones);
+	}
+});
+
+mobileHeaderButton.addEventListener('click', ()=> {
+	mobileHeaderButton.classList.toggle('header__mobile-button-menu-active')
+	document.querySelector('.header').classList.toggle('header_menu-active');
+});
+
+if(smallDisplay.matches) {
+	headerSmallPhoneAdaptation(headerPhonesElements);
+}
+
+const mainSlider = new Splide( '.splide[aria-label="mainSlider"]' );
+// ,
+//       productsSlider = new Splide('.splide[aria-label="productsSlider"]'),
+//       productsButtons = ['Prengi Production', 'Prengi FMC', 'Prengi Mallz', 'Retail Prengi', 'Logistic Prengi', 'IT Prengi HR'];
 
 mainSlider.mount();
 
-productsSlider.on( 'pagination:mounted', function ( data ) {
-    // You can add your class to the UL element
-    data.list.classList.add( 'splide__pagination--custom' );
+// productsSlider.on( 'pagination:mounted', function ( data ) {
+//     data.list.classList.add( 'splide__pagination--custom' );
   
-    // `items` contains all dot items
-    data.items.forEach( function ( item) {
-      item.button.textContent = productsButtons[item.page];
-    } );
-  } );
+//     data.items.forEach( function ( item) {
+//       item.button.textContent = productsButtons[item.page];
+//     } );
+//   } );
   
 
-productsSlider.mount();
+// productsSlider.mount();
